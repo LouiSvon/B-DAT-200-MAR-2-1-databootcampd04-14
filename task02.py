d678bca05855fc32959a8bf5e41ef4ce5f14500d
Louis <louisavon77@gmail.com>
2026-02-12 14:26:29 +0100

    ajout commentaire task02

--- task02.py
+++ task02.py
@@ -1,21 +1,30 @@
-import requests
+import requests 
 
-def groups_count(token: str) -> int :
+def groups_count(token: str) -> int: #prend un token et retourne un entier 
     url = "https://graph.microsoft.com/v1.0/me/memberOf"
+
     headers = {
         "Authorization": f"Bearer {token}",
         "Accept": "application/json",
     }
 
-    count = 0
+    count = 0  #compteur total de groupes
 
     while url:
-        response = requests.get(url, headers=headers)
-        data = response.json()
 
-        values = data.get("value", [])
-        count += len(values)
+        response = requests.get(url, headers=headers) #envoi de la requête HTTP GET vers l’API
 
-        url = data.get("@odata.nextLink")
+        data = response.json() #conversion de la réponse JSON en dictionnaire Python
+
+        values = data.get("value", [])  
+        # "value" contient la liste des groupes sur la page actuelle
+        # Si "value" n’existe pas, on met une liste vide pour éviter une erreur
+
+        count += len(values) #ajoute au compteur le nombre de groupes trouvés
+
+        url = data.get("@odata.nextLink")  
+        #Si l’API indique qu’il existe une page suivante,
+        # @odata.nextLink contient nouvelle URL
+        #Sinon valeur devient None → la boucle s’arrête
 
     return count
