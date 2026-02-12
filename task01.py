f05dbb7acf28e48051028b4cf4a6ed55118f5008
Louis <louisavon77@gmail.com>
2026-02-12 19:45:16 +0100

    modif : task01 ajout parametre mail

--- task01.py
+++ task01.py
@@ -3,12 +3,21 @@ import requests
 def my_mail(token: str) -> str:
     url = "https://graph.microsoft.com/v1.0/me"
 
+    params = {
+        "$select": "mail,userPrincipalName"
+    }
+
     headers = {
         "Authorization": f"Bearer {token}",
     }
 
-    response = requests.get(url, headers=headers)
-
+    response = requests.get(url, headers=headers, params=params)
     data = response.json()
 
-    return data.get("mail") 
+    mail = data.get("mail")
+    upn = data.get("userPrincipalName")
+
+    if mail:
+        return mail
+
+    return upn
